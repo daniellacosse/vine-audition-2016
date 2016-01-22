0e5949949ebf8f83da0a446d0256090ae3a7715e
Daniel LaCosse <d@niellacos.se>
2016-01-21 23:27:42 -0500

    adds test utils

--- test/_util.js
+++ test/_util.js
@@ -1,10 +1,17 @@
-let path = require("path");
 let chai = require("chai");
+let jsdom = require("jsdom").jsdom;
+let path = require("path");
 let TestUtils = require("react-addons-test-utils");
 
 module.exports = {
   load: function(filename) {
     chai.should();
+
+    global.document = jsdom(
+      "<!doctype html><html><body></body></html>"
+    );
+    
+    global.window = document.parentWindow;
 
     return require(
       require("path").join(__dirname, "..", "src/" + filename)
