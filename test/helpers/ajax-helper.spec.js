872f0bd1030b305195421a810e1c206c2377a992
Daniel LaCosse <d@niellacos.se>
2016-01-23 14:39:45 -0500

    write basic ajax helper tests

--- test/helpers/ajax-helper.spec.js
+++ test/helpers/ajax-helper.spec.js
@@ -5,15 +5,30 @@ const JAX = Helper.load("helpers/ajax-helper.js");
 describe("JAX#constructor", () => {
   beforeEach(Helper.startWatch); /* ..and.. */ afterEach(Helper.stopWatch);
 
+  let root = "http://example.org";
+  let config = {
+    get_users: "users"
+  };
+
   it("should construct when given a simple root", () => {
-    true.should.be(false);
+    let _ajax = new JAX(root);
+
+    _ajax.should.not.have.property("get_users");
+    _ajax.should.be.an.instanceof(JAX);
   });
 
   it("should construct when given a vanity method", () => {
-    true.should.be(false);
+    let _ajax = new JAX(root, config);
+
+    _ajax.should.have.property("get_users");
+    _ajax.should.be.an.instanceof(JAX);
   });
 
   it("should fail when no arguments are provided", () => {
-    true.should.be(false);
+    let _f = () => {
+      let _ajax = new JAX();
+    };
+
+    _f.should.throw(TypeError);
   });
 });
