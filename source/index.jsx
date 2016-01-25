bd2f44635d5e524348bcf19e134f0cb86b0e710d
Daniel LaCosse <d@niellacos.se>
2016-01-24 22:54:59 -0500

    index page

--- source/index.jsx
+++ source/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { Router, Route, browserHistory } from "react-router";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
 
 import _i from "./components/_index";
 import { render } from "react-dom";
 
+class Index extends _i.View {
+  render() {
+    return (
+      <section>
+        <h1>AN ISSUE TRACKER FOR NPM</h1>
+        {this.props.children}
+      </section>
+    );
+  }
+}
+
 render(
   (
     <Router history={browserHistory}>
-      <Route path="/" component={_i.Issues}>
+      <Route path="/" component={Index}>
+        <IndexRoute component={_i.Issues} />
       </Route>
     </Router>
   ),
